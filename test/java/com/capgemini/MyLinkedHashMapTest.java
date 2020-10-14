993f4dd6a1dc9db4d87091246ed5eca238112d19
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-15 00:31:40 +0530

    [Shreya]Added:Finding frequency of words in a large sentence using LinkedHashMap

---
+++ test/java/com/capgemini/MyLinkedHashMapTest.java
@@ -0,0 +1,28 @@
+package com.capgemini;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedHashMapTest {
+
+	@Test
+	public void givenASentence_WhenWordsAreAddedTOTheList_ShouldReturn_Paranoid_Frequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myMap.getKey(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myMap.add(word, value);
+		}
+
+		int frequency = myMap.getKey("paranoid");
+		System.out.println(myMap);
+		Assert.assertEquals(3, frequency);
+	}
+
+}
