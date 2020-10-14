879ca90c10a690b9b467a6c7b6d3260e9edff2d9
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-15 01:18:15 +0530

    [Shreya]Added:Binary Search Tree of size 13

--- test/java/com/capgemini/BinarySearchTreeTest.java
+++ test/java/com/capgemini/BinarySearchTreeTest.java
@@ -16,4 +16,25 @@ public class BinarySearchTreeTest {
 		int size=binarySearchTree.getSize();
 		assertTrue(size==3);
 	}
+	
+	@Test
+	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThirteen() {
+		MyBinaryTree<Integer> binarySearchTree=new MyBinaryTree<>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(11);
+		binarySearchTree.add(65);
+		binarySearchTree.add(3);
+		binarySearchTree.add(16);
+		binarySearchTree.add(63);
+		binarySearchTree.add(67);
+		
+		int size=binarySearchTree.getSize();
+		assertTrue(size==13);
+	}
 }
